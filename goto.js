0980d79300248428f89df4d77067e7dc374aa38c
kkn3418690 <kevin3417222@yahoo.com.tw>
2020-10-27 21:35:17 +0800

    global user.name kkn3418690

--- goto.js
+++ goto.js
@@ -3,7 +3,12 @@ $("*").each(function (index, element) {
     $(this).click(function (e) {
         //取得被點選元素的屬性：data-gt-target 
         var target = $(this).attr(data-gt-target);
-        console.log(target)
+        var duration = $(this).attr(data-gt-duration);
+        var offset = $(this).attr(data-gt-offset);
+        console.log("目標 : " + target)
+        console.log("時間 : " + duration)
+        console.log("位移 : " + offset)
+
 
     });
 });
